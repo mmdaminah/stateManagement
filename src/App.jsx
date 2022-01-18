b7ce21917648e9d6d4834bacc01a085b01a7952c
mmdaminah <en.mmdamin@gmail.com>
2022-01-18 19:41:59 +0330

    useState completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,28 @@
-import { useState ,useReducer } from 'react'
-const initialState = {
-  tasks:[]
-}
-const reducer = (state,action)=>{
-  switch(action.type){
-    case 'add':
-      return {...state,tasks:[...state.tasks,action.payload]}
-    case 'delete':
-      return {...state,tasks:state.tasks.filter(task=>task.id !== action.payload)}
-    case 'update':
-      const task = state.tasks.find(item=>item.id === action.payload.idToUpdate)
-      task.name = action.payload.input;
-      return {...state}
-  }
-}
+import { useState } from 'react'
 function App() {
   const [input,setInput] = useState("")
   const [id,setId] = useState(0)
-  const [tasks,dispatch] = useReducer(reducer,initialState)
+  const [tasks,setTasks] = useState([])
   const [idToUpdate,setIdToUpdate] = useState()
+  const [showConfirm,setShowConfirm] = useState(false)
   const handleClick = ()=>{
-    dispatch({type:'add',payload:{id,name:input}})
+    setTasks([...tasks,{id,name:input}])
     setInput("")
     setId(id+1)
   }
   const handleDelete = (id)=>{
-    dispatch({type:'delete',payload:id})
+    setTasks(tasks.filter(task=>task.id !== id))
   }
   const handleUpdate = (task)=>{
     setInput(task.name)
     setIdToUpdate(task.id)
+    setShowConfirm(true)
   }
   const handleUpdateConfirm = ()=>{
-    dispatch({type:'update',payload:{idToUpdate,input}})
+    const task = tasks.find(item=>item.id === idToUpdate)
+    task.name = input;
+    setTasks([...tasks])
+    setShowConfirm(false)
   }
   return (
     <div className="w-full h-full flex flex-col justify-center items-center 
@@ -41,14 +31,18 @@ function App() {
           <label htmlFor="">Task</label>
           <input type="text" placeholder='Entere your Task...' onChange={(e)=>setInput(e.target.value)}
            value={input} className="bg-gray-500 rounded-md mx-2  p-2 outline-none" />
-          <button onClick={handleClick}
+          {!showConfirm &&
+            <button onClick={handleClick}
            className='bg-lime-700 px-4 py-2 rounded-md mx-2'>Add Task</button>
-           <button onClick={handleUpdateConfirm}
-           className='bg-lime-700 px-4 py-2 rounded-md'>Confirm</button>
+           }
+           {showConfirm &&
+             <button onClick={handleUpdateConfirm}
+              className='bg-lime-700 px-4 py-2 rounded-md'>Confirm</button>
+           }
         </div>
         <div className="bg-teal-900 rounded-md overflow-y-auto p-4" style={{width:"40rem",height:"40rem"}}>
         {
-          tasks.tasks.map((task,index)=>(
+          tasks.map((task,index)=>(
             <div key={index} className='flex w-full my-1'>
               <div className='w-2/3'>{task.name}</div>
               <div>
